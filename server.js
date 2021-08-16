e96a71ddeca3903dc2d9244017eb0d9b2102bb09
Adi Kancherla <findadit@gmail.com>
2021-08-15 20:59:31 -0400

    base server 2

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
-const express = require('express');
+const express = require('express')
 const app = express();
+app.use(express.json())
+
+const utils = require("./utils")
 
 app.get('/', (req, res) => {
-  res.send('Hello from server!');
+    res.send('Hello from server!')
 });
 
 // Listen to the App Engine-specified port, or 9090 otherwise
 const PORT = process.env.PORT || 9090;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
+    utils.log(`Server listening on port ${PORT}...`)
+});
+
+app.get('/explore', (req, res) => {
+    res.send('Explore')
+});
+
+app.get('/preview/:item', (req, res) => {
+    res.send('Preview')
+});
+
+app.get('/u/:user/items', (req, res) => {
+    res.send('User items')
+});
+
+app.get('/u/:user/item', (req, res) => {
+    res.send('User item ' +  req.params.user)
 });
---
+++ utils.js
@@ -0,0 +1,69 @@
+const firebaseAdmin = require('firebase-admin')
+
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.applicationDefault()
+})
+
+let DEBUG_LOG = true //todo: change this
+if (process.env.DEBUG_LOG == 'false') {
+  DEBUG_LOG = false
+}
+
+let ERROR_LOG = true
+if (process.env.ERROR_LOG == 'false') {
+  ERROR_LOG = false
+}
+
+module.exports = {
+
+  log: function (obj, ...objs) {
+    if (DEBUG_LOG) {
+      let msg = ""
+      for (const s of objs) {
+        msg += s
+      }
+      console.log('[INFO]: ' + obj + msg)
+    }
+  },
+
+  error: function (obj, ...objs) {
+    if (ERROR_LOG) {
+      let msg = ""
+      for (const s of objs) {
+        msg += s
+      }
+      console.error('[ERROR]: ' + obj + msg)
+    }
+  },
+
+  jsonString: function (obj) {
+    return JSON.stringify(obj, null, 2)
+  },
+
+  authorizeUser: async function (path, token) {
+    // path is in the form /users/userId/*
+    let userId = path.split("/")[2]
+    let checkRevoked = false //todo: see if this needs to be set to true
+    let authenticated = false
+    try {
+      //this.log("Authorizing " + userId + " for " + path)
+      let result = await firebaseAdmin.auth().verifyIdToken(token, checkRevoked)
+      if (result.uid == userId) {
+        authenticated = true
+      }
+    } catch (error) {
+      if (error.code == 'auth/id-token-revoked') {
+        // Token has been revoked. Inform the user to reauthenticate or signOut() the user.
+        this.error("Token is revoked. Please ask user to sign out.", error)
+      } else {
+        // Token is invalid.
+        this.error("Token is invalid. Please ask user to sign in again.", error)
+      }
+    }
+    return authenticated
+  },
+
+  roundToDecimals: function (num, precision) {
+    return Number(Math.round(num + "e+" + precision) + "e-" + precision)
+  }
+}
