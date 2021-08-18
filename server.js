b890cc1b823f2b30dba8e2ab6b550f63ab0c9141
Adi Kancherla <findadit@gmail.com>
2021-08-18 15:21:39 -0400

    mock eps

--- server.js
+++ server.js
@@ -4,28 +4,303 @@ app.use(express.json())
 
 const utils = require("./utils")
 
-app.get('/', (req, res) => {
-    res.send('Hello from server!')
-});
-
 // Listen to the App Engine-specified port, or 9090 otherwise
 const PORT = process.env.PORT || 9090;
 app.listen(PORT, () => {
     utils.log(`Server listening on port ${PORT}...`)
-});
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello from server!')
+})
 
 app.get('/explore', (req, res) => {
-    res.send('Explore')
-});
+    const items = []
+    const item = {}
+    item.image = 'https://nftcompany.com/img/ex.png'
+    item.title = 'Title'
+    item.price = '1 ETH'
+    item.numAvailable = '1 in stock'
+    item.collectionIcon = 'https://nftcompany.com/img/ex.png'
+    item.ownerIcon = 'https://nftcompany.com/img/ex.png'
+    item.creatorIcon = 'https://nftcompany.com/img/ex.png'
+    item.highestBid = '2 ETH'
+    item.timeLeftToBid = '1 hr'
+    items.push(item)
+    res.send(items)
+})
 
-app.get('/preview/:item', (req, res) => {
-    res.send('Preview')
-});
+app.get('/explore/item/:item', (req, res) => {
+    const item = {}
+    item.image = 'https://nftcompany.com/img/ex.png'
+    item.title = 'Title'
+    item.desc = 'Description'
+    item.price = '1 ETH'
+    item.numAvailable = '1 in stock'
+    item.highestBid = '2 ETH'
+    item.highestBidBy = 'abc'
+    item.highestProfileIcon = 'https://nftcompany.com/img/ex.png'
+    item.timeLeftToBid = '1 hr'
+
+    const info = {
+        owner: 'abc',
+        creator: 'abc'
+    }
+    item.info = info
+
+    const owners = [
+        {
+            name: 'abc',
+            profileIcon: 'https://nftcompany.com/img/ex.png'
+        },
+        {
+            name: 'abc',
+            profileIcon: 'https://nftcompany.com/img/ex.png'
+        }
+    ]
+    item.owners = owners
+
+    const history = [
+        {
+            name: 'abc',
+            profileIcon: 'https://nftcompany.com/img/ex.png',
+            timestamp: 123,
+            desc: 'Placed a bid for 1 ETH'
+        },
+        {
+            name: 'abc',
+            profileIcon: 'https://nftcompany.com/img/ex.png',
+            timestamp: 123,
+            desc: 'Put Awesome work on sale'
+        }
+    ]
+    item.history = history
+
+    const bids = [
+        {
+            name: 'abc',
+            profileIcon: 'https://nftcompany.com/img/ex.png',
+            bid: '1.5 ETH'
+        },
+        {
+            name: 'abc',
+            profileIcon: 'https://nftcompany.com/img/ex.png',
+            bid: '1 ETH'
+        }
+    ]
+    item.bids = bids
+
+    res.send(item)
+})
+
+app.get('/u/:user', (req, res) => {
+    const data = {
+        name: 'abc',
+        profileIcon: 'https://nftcompany.com/img/ex.png',
+        location: 'Paris',
+        bio: 'I am an artist',
+        url: 'https://mavrik.me',
+        socialLinks: {
+            twitter: 'https://twitter.com/mavriklabs',
+            insta: 'https://instagram.com/mavriklabs'
+        },
+        memberSince: 'Aug 2021'
+    }
+    res.send(data)
+})
+
+app.get('/u/:user/follows', (req, res) => {
+    const following = [
+        {
+            name: 'abc',
+            profileIcon: 'https://nftcompany.com/img/ex.png',
+            numFollowers: 20,
+            images: [
+                'https://nftcompany.com/img/ex.png',
+                'https://nftcompany.com/img/ex.png'
+            ]
+        },
+        {
+            name: 'abc',
+            profileIcon: 'https://nftcompany.com/img/ex.png',
+            numFollowers: 20,
+            images: [
+                'https://nftcompany.com/img/ex.png',
+                'https://nftcompany.com/img/ex.png'
+            ]
+        }
+    ]
+
+    const followers = [
+        {
+            name: 'abc',
+            profileIcon: 'https://nftcompany.com/img/ex.png',
+            numFollowers: 20,
+            images: [
+                'https://nftcompany.com/img/ex.png',
+                'https://nftcompany.com/img/ex.png'
+            ]
+        },
+        {
+            name: 'abc',
+            profileIcon: 'https://nftcompany.com/img/ex.png',
+            numFollowers: 20,
+            images: [
+                'https://nftcompany.com/img/ex.png',
+                'https://nftcompany.com/img/ex.png'
+            ]
+        }
+    ]
+
+    const data = {
+        followers: followers,
+        following: following
+    }
+
+    res.send(data)
+})
 
 app.get('/u/:user/items', (req, res) => {
-    res.send('User items')
-});
+    const data = {}
 
-app.get('/u/:user/item', (req, res) => {
-    res.send('User item ' +  req.params.user)
-});
+    const onSale = [
+        {
+            image: 'https://nftcompany.com/img/ex.png',
+            title: 'abc',
+            price: '1 ETH',
+            numAvailable: '1/10 in stock',
+            collectionIcon: 'https://nftcompany.com/img/ex.png',
+            ownerIcon: 'https://nftcompany.com/img/ex.png',
+            creatorIcon: 'https://nftcompany.com/img/ex.png',
+            highestBid: '1 ETH',
+            timeLeftToBid: '2 hrs'
+        },
+        {
+            image: 'https://nftcompany.com/img/ex.png',
+            title: 'abc',
+            price: '1 ETH',
+            numAvailable: '1/10 in stock',
+            collectionIcon: 'https://nftcompany.com/img/ex.png',
+            ownerIcon: 'https://nftcompany.com/img/ex.png',
+            creatorIcon: 'https://nftcompany.com/img/ex.png',
+            highestBid: '1 ETH',
+            timeLeftToBid: '2 hrs'
+        }
+    ]
+    data.onSale = onSale
+
+    const collected = [
+        {
+            image: 'https://nftcompany.com/img/ex.png',
+            title: 'abc',
+            price: '1 ETH',
+            collectionIcon: 'https://nftcompany.com/img/ex.png',
+            ownerIcon: 'https://nftcompany.com/img/ex.png',
+            creatorIcon: 'https://nftcompany.com/img/ex.png',
+        },
+        {
+            image: 'https://nftcompany.com/img/ex.png',
+            title: 'abc',
+            price: '1 ETH',
+            collectionIcon: 'https://nftcompany.com/img/ex.png',
+            ownerIcon: 'https://nftcompany.com/img/ex.png',
+            creatorIcon: 'https://nftcompany.com/img/ex.png',
+        }
+    ]
+    data.collected = collected
+
+    const created = [
+        {
+            image: 'https://nftcompany.com/img/ex.png',
+            title: 'abc',
+            price: '1 ETH',
+            numAvailable: '1/10 in stock',
+            collectionIcon: 'https://nftcompany.com/img/ex.png',
+            ownerIcon: 'https://nftcompany.com/img/ex.png',
+            creatorIcon: 'https://nftcompany.com/img/ex.png',
+        },
+        {
+            image: 'https://nftcompany.com/img/ex.png',
+            title: 'abc',
+            price: '1 ETH',
+            numAvailable: '1/10 in stock',
+            collectionIcon: 'https://nftcompany.com/img/ex.png',
+            ownerIcon: 'https://nftcompany.com/img/ex.png',
+            creatorIcon: 'https://nftcompany.com/img/ex.png',
+        }
+    ]
+    data.created = created
+
+    const likes = [
+        {
+            image: 'https://nftcompany.com/img/ex.png',
+            title: 'abc',
+            price: '1 ETH',
+            numAvailable: '1/10 in stock',
+            collectionIcon: 'https://nftcompany.com/img/ex.png',
+            ownerIcon: 'https://nftcompany.com/img/ex.png',
+            creatorIcon: 'https://nftcompany.com/img/ex.png',
+            highestBid: '1 ETH',
+            timeLeftToBid: '2 hrs'
+        },
+        {
+            image: 'https://nftcompany.com/img/ex.png',
+            title: 'abc',
+            price: '1 ETH',
+            numAvailable: '1/10 in stock',
+            collectionIcon: 'https://nftcompany.com/img/ex.png',
+            ownerIcon: 'https://nftcompany.com/img/ex.png',
+            creatorIcon: 'https://nftcompany.com/img/ex.png',
+            highestBid: '1 ETH',
+            timeLeftToBid: '2 hrs'
+        }
+    ]
+    data.likes = likes
+
+    res.send(data)
+})
+
+app.get('/u/:user/item/:item', (req, res) => {
+    const item = {}
+    item.image = 'https://nftcompany.com/img/ex.png'
+    item.title = 'Title'
+    item.desc = 'Description'
+
+    const info = {
+        owner: 'abc',
+        creator: 'abc'
+    }
+    item.info = info
+
+    const history = [
+        {
+            name: 'abc',
+            profileIcon: 'https://nftcompany.com/img/ex.png',
+            timestamp: 123,
+            desc: 'Placed a bid for 1 ETH'
+        },
+        {
+            name: 'abc',
+            profileIcon: 'https://nftcompany.com/img/ex.png',
+            timestamp: 123,
+            desc: 'Put Awesome work on sale'
+        }
+    ]
+    item.history = history
+
+    const bids = [
+        {
+            name: 'abc',
+            profileIcon: 'https://nftcompany.com/img/ex.png',
+            bid: '1.5 ETH'
+        },
+        {
+            name: 'abc',
+            profileIcon: 'https://nftcompany.com/img/ex.png',
+            bid: '1 ETH'
+        }
+    ]
+    item.bids = bids
+
+    res.send(item)
+})
