1281141c2000e1302a0d1e3454ddaa966b15dbb4
Adi Kancherla <findadit@gmail.com>
2021-09-24 18:35:24 -0700

    changed cred file for firebase

--- utils.js
+++ utils.js
@@ -1,17 +1,11 @@
 const firebaseAdmin = require('firebase-admin');
 const { ethers } = require('ethers');
 
-// todo: adi change this before push
-const serviceAccount = require('./creds/nftc-web-firebase-creds.json');
+const serviceAccount = require('./creds/nftc-dev-firebase-creds.json');
 firebaseAdmin.initializeApp({
   // @ts-ignore
   credential: firebaseAdmin.credential.cert(serviceAccount)
 });
-
-// firebaseAdmin.initializeApp({
-//   credential: firebaseAdmin.credential.applicationDefault()
-// })
-
 Object.defineProperty(global, '__stack', {
   get: function () {
     const orig = Error.prepareStackTrace;
