3b24d699fa219175e2f99f1e9e1bc3975870c06a
Adi Kancherla <findadit@gmail.com>
2021-10-06 11:39:13 -0700

    check for invalid/contract addresses on opensea

--- openseaData.js
+++ openseaData.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
 const { readFile } = require('fs').promises;
+const { appendFileSync } = require('fs');
 const { promisify } = require('util');
 const parse = promisify(require('csv-parse'));
+
+const { ethers } = require('ethers');
+const ethersProvider = new ethers.providers.JsonRpcProvider(process.env.alchemyJsonRpcEthMainnet);
+
 const firebaseAdmin = require('firebase-admin');
 
 const serviceAccount = require('./creds/nftc-infinity-firebase-creds.json');
@@ -19,14 +25,43 @@ const db = firebaseAdmin.firestore();
 async function importCsv(csvFileName) {
   const fileContents = await readFile(csvFileName, 'utf8');
   // @ts-ignore
-  const records = await parse(fileContents, { columns: true });
+  const records = await parse(fileContents, { columns: false });
   try {
-    await writeToFirestore(records);
+    // await writeToFirestore(records);
+    await removeInvalidAddresses(records);
   } catch (e) {
     console.error(e);
     process.exit(1);
   }
-  console.log(`Wrote ${records.length} records`);
+  console.log(`Processed ${records.length} records`);
+}
+
+async function removeInvalidAddresses(records) {
+  for (let i = 0; i < records.length; i++) {
+    console.log(i);
+    if (i > 999) {
+        break;
+    }
+    const record = records[i];
+    const address = record[0];
+    const total = record[1];
+    const isValid = ethers.utils.isAddress(address);
+    if (isValid) {
+      ethersProvider.getCode(address).then((res) => {
+        if (res === '0x') {
+          console.log('normal');
+          appendFileSync('./normal', address + ',' + total + '\n');
+        } else {
+          console.log('contract');
+          appendFileSync('./contract', address + ',' + total + '\n');
+        }
+      });
+    } else {
+      console.log('invalid');
+      appendFileSync('./invalid', address + ',' + total + '\n');
+    }
+  }
+  // records.forEach(async (record, i) => {});
 }
 
 function writeToFirestore(records) {
