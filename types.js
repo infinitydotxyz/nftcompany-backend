af9ac38d6bf8af804371db3c530fdd3e13a5180a
Adi Kancherla <findadit@gmail.com>
2021-10-06 23:18:19 -0700

    gg

--- types.js
+++ types.js
@@ -8,31 +8,31 @@ const RewardTiers = {
     min: 0,
     max: 1000,
     eligible: 706,
-    threshold: 0.03
+    threshold: 0.02
   },
   t2: {
     min: 1000,
     max: 20000,
     eligible: 2193,
-    threshold: 0.6
+    threshold: 0.5
   },
   t3: {
     min: 20000,
     max: 100000,
     eligible: 6587,
-    threshold: 3
+    threshold: 2
   },
   t4: {
     min: 100000,
     max: 500000,
     eligible: 14386,
-    threshold: 15
+    threshold: 10
   },
   t5: {
     min: 500000,
     max: Infinity,
     eligible: 34180,
-    threshold: 30
+    threshold: 20
   }
 };
 
