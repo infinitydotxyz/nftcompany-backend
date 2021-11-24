3db3e175f83caa7e3edaac5a9c26ca657eb35904
Adi Kancherla <findadit@gmail.com>
2021-11-24 10:22:21 -0500

    search title and coll name

--- firestore.js
+++ firestore.js
@@ -689,6 +689,91 @@ async function updateListingTypeHelper(startAfterCreatedAt, limit) {
   await Promise.all(batchCommits);
 }
 
+async function updateSearchTitleAndCollName(csvFileName) {
+  try {
+    const limit = 500;
+
+    if (readComplete) {
+      console.log('totalListings', totalListings);
+      return;
+    }
+    console.log('============================================================================');
+    console.log('num recurses', ++numRecurses);
+
+    const fileContents = await readFile(csvFileName, 'utf8');
+    // @ts-ignore
+    const records = await parse(fileContents, { columns: false });
+    let startAfterCreatedAt = records[0][1];
+    if (!startAfterCreatedAt) {
+      startAfterCreatedAt = Date.now();
+    }
+    await updateSearchTitleAndCollNameHelper(+startAfterCreatedAt, limit);
+    await updateSearchTitleAndCollName(csvFileName);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+async function updateSearchTitleAndCollNameHelper(startAfterCreatedAt, limit) {
+  console.log('starting after', startAfterCreatedAt);
+  const batchCommits = [];
+  let batch = db.batch();
+
+  const query = db
+    .collectionGroup(fstrCnstnts.LISTINGS_COLL)
+    .orderBy('metadata.createdAt', 'desc')
+    .startAfter(startAfterCreatedAt)
+    .limit(limit);
+  const snapshot = await query.get();
+
+  if (snapshot.docs.length < limit) {
+    readComplete = true;
+  }
+
+  totalListings += snapshot.docs.length;
+  console.log('totalListings so far', totalListings);
+
+  try {
+    for (let i = 0; i < snapshot.docs.length; i++) {
+      const doc = snapshot.docs[i];
+      const ref = doc.ref;
+      const data = doc.data();
+
+      if ((i + 1) % limit === 0) {
+        writeFileSync('./lastItem', `${doc.id},${data.metadata.createdAt}\n`);
+      }
+
+      let searchTitle = data.metadata.asset.searchTitle;
+      let searchCollectionName = data.metadata.asset.searchCollectionName;
+
+      searchTitle = searchTitle && searchTitle.replace(/[\s-_]/g, '').toLowerCase();
+      searchCollectionName = searchCollectionName && searchCollectionName.replace(/[\s-_]/g, '').toLowerCase();
+
+      const obj = {
+        metadata: {
+          asset: {
+            searchTitle,
+            searchCollectionName
+          }
+        }
+      };
+
+      batch.set(ref, obj, { merge: true });
+
+      if ((i + 1) % limit === 0) {
+        console.log(`Writing record ${i + 1}`);
+        batchCommits.push(batch.commit());
+        batch = db.batch();
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  batchCommits.push(batch.commit());
+  await Promise.all(batchCommits);
+}
+
 // ===================================================== MAINS ==========================================================
 
 // main(process.argv[2]).catch((e) => console.error(e));
@@ -701,7 +786,9 @@ async function updateListingTypeHelper(startAfterCreatedAt, limit) {
 
 // updateTraits(process.argv[2]).catch((e) => console.error(e));
 
-updateListingType(process.argv[2]).catch((e) => console.error(e));
+// updateListingType(process.argv[2]).catch((e) => console.error(e));
+
+updateSearchTitleAndCollName(process.argv[2]).catch((e) => console.error(e));
 
 // =================================================== HELPERS ===========================================================
 
