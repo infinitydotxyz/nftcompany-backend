5e3ac80cccc92e7e9050e0b5cd99b9c616f5489d
Adi Kancherla <findadit@gmail.com>
2021-11-27 15:26:58 -0500

    minor

--- firestore.js
+++ firestore.js
@@ -831,8 +831,7 @@ async function updateChainIdInListingsHelper(startAfterCreatedAt, limit) {
 
       const obj = {
         metadata: {
-          chainId: '1',
-          chain: 'Ethereum'
+          chainId: '1'
         }
       };
 
@@ -908,8 +907,7 @@ async function updateChainIdInAllCollsHelper(startAfterName, limit) {
       }
 
       const obj = {
-        chainId: '1',
-        chain: 'Ethereum'
+        chainId: '1'
       };
 
       batch.set(ref, obj, { merge: true });
@@ -984,8 +982,82 @@ async function updateChainIdInTxnsHelper(startAfterCreatedAt, limit) {
       }
 
       const obj = {
-        chainId: '1',
-        chain: 'Ethereum'
+        chainId: '1'
+      };
+
+      batch.set(ref, obj, { merge: true });
+
+      if ((i + 1) % limit === 0) {
+        console.log(`Writing record ${i + 1}`);
+        batchCommits.push(batch.commit());
+        batch = db.batch();
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  batchCommits.push(batch.commit());
+  await Promise.all(batchCommits);
+}
+
+let totalOffers = 0;
+async function updateChainIdInOffers(csvFileName) {
+  try {
+    const limit = 500;
+
+    if (readComplete) {
+      console.log('totalOffers', totalOffers);
+      return;
+    }
+    console.log('============================================================================');
+    console.log('num recurses', ++numRecurses);
+
+    const fileContents = await readFile(csvFileName, 'utf8');
+    // @ts-ignore
+    const records = await parse(fileContents, { columns: false });
+    let startAfterCreatedAt = records[0][1];
+    if (!startAfterCreatedAt) {
+      startAfterCreatedAt = Date.now();
+    }
+    await updateChainIdInOffersHelper(+startAfterCreatedAt, limit);
+    await updateChainIdInOffers(csvFileName);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+async function updateChainIdInOffersHelper(startAfterCreatedAt, limit) {
+  console.log('starting after', startAfterCreatedAt);
+  const batchCommits = [];
+  let batch = db.batch();
+
+  const query = db
+    .collectionGroup(fstrCnstnts.OFFERS_COLL)
+    .orderBy('createdAt', 'desc')
+    .startAfter(startAfterCreatedAt)
+    .limit(limit);
+  const snapshot = await query.get();
+
+  if (snapshot.docs.length < limit) {
+    readComplete = true;
+  }
+
+  totalOffers += snapshot.docs.length;
+  console.log('totalOffers so far', totalOffers);
+
+  try {
+    for (let i = 0; i < snapshot.docs.length; i++) {
+      const doc = snapshot.docs[i];
+      const ref = doc.ref;
+      const data = doc.data();
+
+      if ((i + 1) % limit === 0) {
+        writeFileSync('./lastItem', `${doc.id},${data.createdAt}\n`);
+      }
+
+      const obj = {
+        chainId: '1'
       };
 
       batch.set(ref, obj, { merge: true });
@@ -1023,7 +1095,9 @@ async function updateChainIdInTxnsHelper(startAfterCreatedAt, limit) {
 
 // updateChainIdInAllColls(process.argv[2]).catch((e) => console.error(e));
 
-updateChainIdInTxns(process.argv[2]).catch((e) => console.error(e));
+// updateChainIdInTxns(process.argv[2]).catch((e) => console.error(e));
+
+updateChainIdInOffers(process.argv[2]).catch((e) => console.error(e));
 
 // =================================================== HELPERS ===========================================================
 
