8e562abcf4287e9689e4c0d1b04f9cdce27142cf
Adi Kancherla <findadit@gmail.com>
2021-11-30 02:51:59 -0600

    fix

--- server.js
+++ server.js
@@ -2495,6 +2495,7 @@ async function postListing(maker, payload, batch, numOrders, hasBonus) {
           metadata: {
             hasBlueCheck: payload.metadata.hasBlueCheck,
             schema: payload.metadata.schema,
+            chainId,
             asset: {
               address: tokenAddress,
               collectionName: payload.metadata.asset.collectionName,
