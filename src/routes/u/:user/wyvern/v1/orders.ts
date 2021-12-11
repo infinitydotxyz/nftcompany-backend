e91ec16237710162f0c5d0d52188c4235774e51f
Joe <jfrazier153@outlook.com>
2021-12-11 12:46:05 -0600

    refactored user orders ep to use service helper functions

--- src/routes/u/:user/wyvern/v1/orders.ts
+++ src/routes/u/:user/wyvern/v1/orders.ts
@@ -1,13 +1,12 @@
 import { firestore } from '@base/container';
+import { OrderSide } from '@base/types/NftInterface';
 import { StatusCode } from '@base/types/StatusCode';
-import { fstrCnstnts, NFTC_FEE_ADDRESS } from '@constants';
+import { NFTC_FEE_ADDRESS } from '@constants';
 import { hasBonusReward } from '@services/infinity/collections/hasBonusReward';
-import { isTokenVerified } from '@services/infinity/collections/isTokenVerified';
-import { updateNumOrders } from '@services/infinity/orders/updateNumOrders';
+import { postListing } from '@services/infinity/listings/postListing';
+import { postOffer } from '@services/infinity/offers/postOffer';
 import { error, log } from '@utils/logger';
 import { Request, Response } from 'express';
-import firebaseAdmin from 'firebase-admin';
-import { prepareEmail } from '../../reward';
 
 // post a listing or make offer
 export const postUserOrders = async (req: Request<{ user: string }>, res: Response) => {
@@ -69,10 +68,10 @@ export const postUserOrders = async (req: Request<{ user: string }>, res: Respon
     }
     payload.metadata.hasBonusReward = hasBonus;
 
-    if (payload.side === 1) {
+    if (payload.side === OrderSide.Sell) {
       // listing
       await postListing(maker, payload, batch, numOrders, hasBonus);
-    } else if (payload.side === 0) {
+    } else if (payload.side === OrderSide.Buy) {
       // offer
       await postOffer(maker, payload, batch, numOrders, hasBonus);
     } else {
@@ -83,112 +82,11 @@ export const postUserOrders = async (req: Request<{ user: string }>, res: Respon
 
     // commit batch
     log('Committing post order batch to firestore');
-    batch
-      .commit()
-      .then((resp: any) => {
-        res.send(payload);
-      })
-      .catch((err: Error) => {
-        error('Failed to post order');
-        error(err);
-        res.sendStatus(StatusCode.InternalServerError);
-      });
+    await batch.commit();
+    res.send(payload);
+    return;
   } catch (err) {
     error(err);
     res.sendStatus(StatusCode.InternalServerError);
   }
 };
-
-export async function postListing(maker: string, payload: any, batch: any, numOrders: number, hasBonus: boolean) {
-  log('Writing listing to firestore for user + ' + maker);
-  const { basePrice } = payload;
-  const tokenAddress = payload.metadata.asset.address.trim().toLowerCase();
-  const tokenId = payload.metadata.asset.id.trim();
-  const chainId = payload.metadata.chainId;
-
-  // check if token is verified if payload instructs so
-  let blueCheck = payload.metadata.hasBlueCheck;
-  if (payload.metadata.checkBlueCheck) {
-    blueCheck = await isTokenVerified(tokenAddress);
-  }
-  payload.metadata.hasBlueCheck = blueCheck;
-  payload.metadata.createdAt = Date.now();
-
-  // write listing
-  const listingRef = firestore
-    .collection(fstrCnstnts.ROOT_COLL)
-    .doc(fstrCnstnts.INFO_DOC)
-    .collection(fstrCnstnts.USERS_COLL)
-    .doc(maker)
-    .collection(fstrCnstnts.LISTINGS_COLL)
-    .doc(firestore.getDocId({ tokenAddress, tokenId, basePrice }));
-
-  batch.set(listingRef, payload, { merge: true });
-
-  // store as asset for future use
-  const listingAsAssetRef = firestore
-    .collection(fstrCnstnts.ROOT_COLL)
-    .doc(fstrCnstnts.INFO_DOC)
-    .collection(fstrCnstnts.ASSETS_COLL)
-    .doc(firestore.getAssetDocId({ tokenAddress, tokenId, chainId }));
-
-  batch.set(listingAsAssetRef, payload, { merge: true });
-
-  // update collection listings
-  try {
-    firestore
-      .collection(fstrCnstnts.COLLECTION_LISTINGS_COLL)
-      .doc(tokenAddress)
-      .set(
-        {
-          numListings: firebaseAdmin.firestore.FieldValue.increment(numOrders),
-          metadata: {
-            hasBlueCheck: payload.metadata.hasBlueCheck,
-            schema: payload.metadata.schema,
-            chainId,
-            asset: {
-              address: tokenAddress,
-              collectionName: payload.metadata.asset.collectionName,
-              searchCollectionName: payload.metadata.asset.searchCollectionName,
-              description: payload.metadata.asset.description,
-              image: payload.metadata.asset.image,
-              imagePreview: payload.metadata.asset.imagePreview
-            }
-          }
-        },
-        { merge: true }
-      );
-  } catch (err) {
-    error('Error updating root collection data on post listing');
-    error(err);
-  }
-
-  // update num user listings
-  updateNumOrders(batch, maker, numOrders, hasBonus, 1);
-}
-
-export async function postOffer(maker: string, payload: any, batch: any, numOrders: number, hasBonus: boolean) {
-  log('Writing offer to firestore for user', maker);
-  const taker = payload.metadata.asset.owner.trim().toLowerCase();
-  const { basePrice } = payload;
-  const tokenAddress = payload.metadata.asset.address.trim().toLowerCase();
-  const tokenId = payload.metadata.asset.id.trim();
-  payload.metadata.createdAt = Date.now();
-
-  // store data in offers of maker
-  const offerRef = firestore
-    .collection(fstrCnstnts.ROOT_COLL)
-    .doc(fstrCnstnts.INFO_DOC)
-    .collection(fstrCnstnts.USERS_COLL)
-    .doc(maker)
-    .collection(fstrCnstnts.OFFERS_COLL)
-    .doc(firestore.getDocId({ tokenAddress, tokenId, basePrice }));
-  batch.set(offerRef, payload, { merge: true });
-
-  log('updating num offers since offer does not exist');
-  // update num user offers made
-  updateNumOrders(batch, maker, numOrders, hasBonus, 0);
-
-  // send email to taker that an offer is made
-  prepareEmail(taker, payload, 'offerMade');
-}
---
+++ src/services/infinity/offers/postOffer.ts
@@ -0,0 +1,31 @@
+import { firestore } from '@base/container';
+import { fstrCnstnts } from '@constants';
+import { prepareEmail } from '@routes/u/:user/reward';
+import { log } from '@utils/logger';
+import { updateNumOrders } from '../orders/updateNumOrders';
+
+export async function postOffer(maker: string, payload: any, batch: any, numOrders: number, hasBonus: boolean) {
+  log('Writing offer to firestore for user', maker);
+  const taker = payload.metadata.asset.owner.trim().toLowerCase();
+  const { basePrice } = payload;
+  const tokenAddress = payload.metadata.asset.address.trim().toLowerCase();
+  const tokenId = payload.metadata.asset.id.trim();
+  payload.metadata.createdAt = Date.now();
+
+  // store data in offers of maker
+  const offerRef = firestore
+    .collection(fstrCnstnts.ROOT_COLL)
+    .doc(fstrCnstnts.INFO_DOC)
+    .collection(fstrCnstnts.USERS_COLL)
+    .doc(maker)
+    .collection(fstrCnstnts.OFFERS_COLL)
+    .doc(firestore.getDocId({ tokenAddress, tokenId, basePrice }));
+  batch.set(offerRef, payload, { merge: true });
+
+  log('updating num offers since offer does not exist');
+  // update num user offers made
+  updateNumOrders(batch, maker, numOrders, hasBonus, 0);
+
+  // send email to taker that an offer is made
+  prepareEmail(taker, payload, 'offerMade');
+}
