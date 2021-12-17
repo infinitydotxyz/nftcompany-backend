3987fb361afc4f7cc07947be0d6fdc48178e5aa8
Adi Kancherla <findadit@gmail.com>
2021-12-16 18:17:18 -0600

    minor

--- utils.js
+++ utils.js
@@ -12,7 +12,7 @@ const serviceAccount = require('./creds/nftc-dev-firebase-creds.json');
 firebaseAdmin.initializeApp({
   // @ts-ignore
   credential: firebaseAdmin.credential.cert(serviceAccount),
-  storageBucket: 'nftc-dev.appspot.com' // todo: adi change this in release
+  storageBucket: 'nftc-dev.appspot.com'
 });
 
 Object.defineProperty(global, '__stack', {
