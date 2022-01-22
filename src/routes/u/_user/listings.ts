5112c54b55c60adc59a821e8385d9c0cea91e78b
ngduc <nducus@yahoo.com>
2022-01-22 10:28:34 +0000

    getUserListings: support filters: price, listType

--- src/routes/u/_user/listings.ts
+++ src/routes/u/_user/listings.ts
@@ -8,6 +8,7 @@ import { Request, Response } from 'express';
 
 // fetch listings of user
 export const getUserListings = async (req: Request<{ user: string }>, res: Response) => {
+  const { listType, priceMin = '', priceMax = '' } = req.query;
   const user = (`${req.params.user}` || '').trim().toLowerCase();
   const queries = parseQueryFields(res, req, ['limit', 'startAfterMillis'], ['50', `${Date.now()}`]);
   if ('error' in queries) {
@@ -20,11 +21,30 @@ export const getUserListings = async (req: Request<{ user: string }>, res: Respo
   }
 
   try {
-    const data = await getUserListingsRef(user)
-      .orderBy('metadata.createdAt', OrderDirection.Descending)
-      .startAfter(queries.startAfterMillis)
-      .limit(queries.limit)
-      .get();
+    let queryRef: FirebaseFirestore.Query<FirebaseFirestore.DocumentData> = getUserListingsRef(user);
+
+    if (priceMin) {
+      queryRef = queryRef.where('metadata.basePriceInEth', '>=', +priceMin);
+    }
+    if (priceMax) {
+      queryRef = queryRef.where('metadata.basePriceInEth', '<=', +priceMax);
+    }
+
+    if (listType) {
+      queryRef = queryRef.where('metadata.listingType', '==', listType);
+    }
+
+    if (+priceMin || +priceMax) {
+      queryRef = queryRef
+        .orderBy('metadata.basePriceInEth', OrderDirection.Ascending)
+        .orderBy('metadata.createdAt', OrderDirection.Descending)
+        .startAfter(0, queries.startAfterMillis); // orderBy & startAfter fields should match.
+    } else {
+      queryRef = queryRef.orderBy('metadata.createdAt', OrderDirection.Descending).startAfter(queries.startAfterMillis);
+    }
+
+    queryRef = queryRef.limit(queries.limit);
+    const data = await queryRef.get();
     const resp = getOrdersResponse(data);
     res.send(resp);
   } catch (err) {
