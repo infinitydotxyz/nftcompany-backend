fc8b460d810d7a0ff7d334458cebdf303152bb96
Adi Kancherla <findadit@gmail.com>
2022-02-03 09:53:06 -0500

    makes alchemyClients singletons

--- src/services/alchemy/utils.ts
+++ src/services/alchemy/utils.ts
@@ -2,18 +2,23 @@ import axios, { AxiosInstance } from 'axios';
 import { ALCHEMY_NFT_BASE_URL_ETH_MAINNET, ALCHEMY_NFT_BASE_URL_POLYGON_MAINNET } from '@base/constants';
 import { alchemyParamSerializer } from '@base/utils/formatters';
 
-export function getAlchemyClient(chainId: string): AxiosInstance {
-  return axios.create({
-    baseURL: getAlchemyBaseUrl(chainId),
-    paramsSerializer: alchemyParamSerializer
-  });
-}
+// eth mainnet
+export const alchemyMainnetClient = axios.create({
+  baseURL: ALCHEMY_NFT_BASE_URL_ETH_MAINNET,
+  paramsSerializer: alchemyParamSerializer
+});
 
-export function getAlchemyBaseUrl(chainId: string): string {
+// polygon mainnet
+export const alchemyPolygonClient = axios.create({
+  baseURL: ALCHEMY_NFT_BASE_URL_POLYGON_MAINNET,
+  paramsSerializer: alchemyParamSerializer
+});
+
+export function getAlchemyClient(chainId: string): AxiosInstance {
   if (chainId === '1') {
-    return ALCHEMY_NFT_BASE_URL_ETH_MAINNET;
+    return alchemyMainnetClient;
   } else if (chainId === '137') {
-    return ALCHEMY_NFT_BASE_URL_POLYGON_MAINNET;
+    return alchemyPolygonClient;
   }
   throw Error('Unknown chainId. Alchemy does not recognize chainId: ' + chainId);
 }
