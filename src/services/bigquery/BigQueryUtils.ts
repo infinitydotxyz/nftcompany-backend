1edc3ee46603614a7def3bf944603ba98b9b1409
ngduc <nducus@yahoo.com>
2022-03-11 00:13:38 -0800

    clean up

--- src/services/bigquery/BigQueryUtils.ts
+++ src/services/bigquery/BigQueryUtils.ts
@@ -1,36 +1,11 @@
 import path from 'path'
 import { BigQuery } from '@google-cloud/bigquery';
-import { fetchUserFollows } from 'routes/u/_user/collectionFollows';
-import { trimLowerCase } from '@infinityxyz/lib/utils';
 
 const options = {
   keyFilename: path.join(__dirname, '../../../creds/nftc-dev-bigquery-creds.json'),
   projectId: 'nftc-dev'
 };
 const bigquery = new BigQuery(options);
-
-export async function init() {
-  const user = trimLowerCase('0x006fA88c8b4C9D60393498Fd1b2ACf6abE254d72');
-  const follows = await fetchUserFollows(user, 9999);
-  const followAddresses = follows.map((item) => `'${item.address}'`).join(','); // 'addr1', 'addr2'
-
-  const query = `SELECT
-      JSON_EXTRACT_SCALAR(DATA,
-        "$.collectionAddress") AS collectionAddr,
-      CAST(JSON_EXTRACT_SCALAR(DATA,
-          "$.price") AS FLOAT64) AS price,
-      DATA
-    FROM
-      \`nftc-dev.fs_mirror_sales.sales_raw_latest\`
-    WHERE
-        JSON_EXTRACT_SCALAR(DATA, "$.collectionAddress") IN (${followAddresses})
-    ORDER BY CAST(JSON_EXTRACT_SCALAR(DATA, "$.blockTimestamp") AS INT64)
-    LIMIT 1000
-  `;
-  // console.log('query', query)
-  const rows = await queryBigData(query);
-  // console.log('rows', rows)
-}
 
 export async function queryBigData(query: string, datasetId = 'fs_mirror_sales', tableId = 'q_sales_by_collections') {
   const options = {
