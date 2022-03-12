fb0db785df2b0a2267b2ce2d95d62b0a5f869083
ngduc <nducus@yahoo.com>
2022-03-11 23:55:43 -0800

    query: combine sales and feed events

--- src/routes/u/_user/userFeed.ts
+++ src/routes/u/_user/userFeed.ts
@@ -27,20 +27,36 @@ export const getUserFeed = async (
   const follows = await fetchUserFollows(user, QUERY_BY_COLLECTIONS_LIMIT);
   const followAddresses = follows.map((item) => `'${item.address}'`).join(','); // 'addr1', 'addr2'
 
-  const query = `SELECT
-      JSON_EXTRACT_SCALAR(DATA, "$.chainId") AS chainId,
-      JSON_EXTRACT_SCALAR(DATA, "$.collectionAddress") AS tokenAddress,
-      JSON_EXTRACT_SCALAR(DATA, "$.tokenId") AS tokenId,
-      JSON_EXTRACT_SCALAR(DATA, "$.buyer") AS buyer,
-      JSON_EXTRACT_SCALAR(DATA, "$.seller") AS seller,
-      JSON_EXTRACT_SCALAR(DATA, "$.price") AS price,
-      JSON_EXTRACT_SCALAR(DATA, "$.blockTimestamp") AS timestamp
-    FROM
-      \`nftc-dev.fs_mirror_sales.sales_raw_latest\`
-    WHERE
-        JSON_EXTRACT_SCALAR(DATA, "$.collectionAddress") IN (${followAddresses})
-    ORDER BY CAST(JSON_EXTRACT_SCALAR(DATA, "$.blockTimestamp") AS INT64)
-    LIMIT ${limit}
+  const query = `
+SELECT
+  JSON_EXTRACT_SCALAR(DATA, "$.collectionAddress") AS tokenAddress,
+  JSON_EXTRACT_SCALAR(DATA, "$.tokenId") AS tokenId,
+  JSON_EXTRACT_SCALAR(DATA, "$.buyer") AS buyer,
+  JSON_EXTRACT_SCALAR(DATA, "$.seller") AS seller,
+  JSON_EXTRACT_SCALAR(DATA, "$.price") AS price,
+  JSON_EXTRACT_SCALAR(DATA, "$.blockTimestamp") AS blockTimestamp,
+  "" AS type
+  -- CAST(JSON_EXTRACT_SCALAR(DATA,
+  --     "$.price") AS FLOAT64) AS price
+FROM
+  \`nftc-dev.fs_mirror_sales.sales_raw_latest\`
+  WHERE JSON_EXTRACT_SCALAR(DATA, "$.collectionAddress") IN (${followAddresses})
+
+UNION ALL
+
+SELECT
+JSON_EXTRACT_SCALAR(data, "$.tokenAddress") AS tokenAddress,
+"" AS tokenId,
+"" AS buyer,
+"" AS seller,
+"" AS price,
+"" AS blockTimestamp,
+JSON_EXTRACT_SCALAR(data, "$.type") AS type
+FROM
+\`nftc-dev.fs_mirror_feed.feed_raw_latest\`
+WHERE JSON_EXTRACT_SCALAR(DATA, "$.tokenAddress") IN (${followAddresses})
+
+LIMIT ${limit}
   `;
   // console.log('query', query)
   const rows = await queryBigData(query);
