4349bfad1eaefbfd11a42cf8686f3ee9499734b9
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-03-29 22:51:50 -0500

    removed old dependencies

--- src/container.ts
+++
@@ -1,9 +0,0 @@
-// Import Firestore from './databases/Firestore';
-import { container } from 'tsyringe';
-
-class Firestore {}
-
-/**
- * Hack to make this not complain
- */
-export const firestore: any = container.resolve(Firestore);
