1f1a468370075a8d7d750145ef91ba2ddf412294
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-03-31 12:23:53 -0500

    added endpoint to get twitter mentions ordered by author followers

--- src/collections/collections.module.ts
+++ src/collections/collections.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { StatsModule } from 'stats/stats.module';
+import { TwitterModule } from 'twitter/twitter.module';
 import { VotesModule } from 'votes/votes.module';
 import { CollectionsController } from './collections.controller';
 import CollectionsService from './collections.service';
@@ -7,7 +8,7 @@ import { NftsController } from './nfts/nfts.controller';
 import { NftsService } from './nfts/nfts.service';
 
 @Module({
-  imports: [StatsModule, VotesModule],
+  imports: [StatsModule, VotesModule, TwitterModule],
   providers: [CollectionsService, NftsService],
   controllers: [CollectionsController, NftsController]
 })
---
+++ src/twitter/dto/tweet.dto.ts
@@ -0,0 +1,30 @@
+import { InfinityTweet } from '@infinityxyz/lib/types/services/twitter';
+import { ApiProperty } from '@nestjs/swagger';
+import { TwitterAccountDto } from './twitter-account.dto';
+
+export class TweetDto implements InfinityTweet {
+  @ApiProperty({
+    description: 'Author of the tweet'
+  })
+  author: TwitterAccountDto;
+
+  @ApiProperty({
+    description: 'Epoch timestamp (ms) of when the tweet was created'
+  })
+  createdAt: number;
+
+  @ApiProperty({
+    description: 'Unique identifier of the tweet'
+  })
+  tweetId: string;
+
+  @ApiProperty({
+    description: 'Content of the tweet'
+  })
+  text: string;
+
+  @ApiProperty({
+    description: 'Link to the tweet'
+  })
+  url: string;
+}
