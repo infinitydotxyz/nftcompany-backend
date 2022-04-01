57758cd6ed44ee48875d2e3e757ed346059ea659
Sleeyax <yourd3veloper@gmail.com>
2022-04-01 16:00:06 +0200

    fix user id example

--- src/common/decorators/param-user-id.decorator.ts
+++ src/common/decorators/param-user-id.decorator.ts
@@ -13,10 +13,12 @@ export const ParamUserId = createParamDecorator(
         name: 'id',
         description: 'The id of the user',
         required: true,
-        example: {
-          summary: 'Via chain id and address',
-          description: 'Identify a user their wallet chain id and address. Format <chainId:address>',
-          value: '1:0xd8da6bf26964af9d7eed9e03e53415d37aa96045'
+        examples: {
+          address: {
+            summary: 'Via chain id and address',
+            description: 'Identify a user their wallet chain id and address. Format <chainId:address>',
+            value: '1:0xd8da6bf26964af9d7eed9e03e53415d37aa96045'
+          }
         },
         schema: { type: 'string' }
       })(target, key, Object.getOwnPropertyDescriptor(target, key));
