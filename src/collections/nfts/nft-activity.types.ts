990257407faaab7f1127250aa50c106a41da39ed
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-04-15 14:10:03 -0500

    added support for listing and offer to nft activity query

--- src/collections/nfts/nft-activity.types.ts
+++ src/collections/nfts/nft-activity.types.ts
@@ -1,11 +1,13 @@
 import { FeedEventType } from '@infinityxyz/lib/types/core/feed';
 
 export enum ActivityType {
-  Sale = 'sale'
-  // Listing = 'listing'
-  // Offer = 'offer'
+  Sale = 'sale',
+  Listing = 'listing',
+  Offer = 'offer'
 }
 
 export const activityTypeToEventType = {
-  [ActivityType.Sale]: FeedEventType.NftSale
+  [ActivityType.Sale]: FeedEventType.NftSale,
+  [ActivityType.Listing]: FeedEventType.NftListing,
+  [ActivityType.Offer]: FeedEventType.NftOffer
 };
