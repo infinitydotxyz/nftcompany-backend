699fcb8fd04f2b2cadfc0250fee1e6de669990c0
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-04-29 15:39:01 -0500

    get provider url from .env file instead of api key

--- src/ethereum/ethereum.service.ts
+++ src/ethereum/ethereum.service.ts
@@ -2,20 +2,27 @@ import { ChainId } from '@infinityxyz/lib/types/core';
 import { trimLowerCase } from '@infinityxyz/lib/utils/formatters';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config/dist/config.service';
-import { ERC721ABI } from 'abi/erc721';
+import { ERC721ABI } from '../abi/erc721';
 import { ethers } from 'ethers';
-import { join } from 'path';
-import { EnvironmentVariables } from 'types/environment-variables.interface';
+import { EnvironmentVariables } from '../types/environment-variables.interface';
 
 @Injectable()
 export class EthereumService {
   private _providers: Map<ChainId, ethers.providers.JsonRpcProvider> = new Map();
 
   constructor(private configService: ConfigService<EnvironmentVariables>) {
-    const alchemyApiKey = this.configService.get('ALCHEMY_API_KEY');
+    const mainnetUrl = this.configService.get('alchemyJsonRpcEthMainnet');
+    const polygonUrl = this.configService.get('alchemyJsonRpcPolygonMainnet');
+    const providerUrlByChainId = {
+      [ChainId.Mainnet]: mainnetUrl,
+      [ChainId.Polygon]: polygonUrl
+    };
 
     for (const chainId of Object.values(ChainId)) {
-      const providerUrl = this.getProviderUrl(chainId, alchemyApiKey);
+      const providerUrl = providerUrlByChainId[chainId];
+      if (!providerUrl) {
+        throw new Error(`Provider is not configured for chainId: ${chainId}`);
+      }
       this._providers.set(chainId, new ethers.providers.JsonRpcProvider(providerUrl));
     }
   }
@@ -34,21 +41,5 @@ export class EthereumService {
     const contract = new ethers.Contract(token.address, ERC721ABI, provider);
     const owner = trimLowerCase(await contract.ownerOf(token.tokenId));
     return owner;
-  }
-
-  private getProviderUrl(chainId: ChainId, apiKey: string) {
-    const baseUrlByChainId = {
-      [ChainId.Mainnet]: `https://eth-mainnet.alchemyapi.io/v2/`,
-      [ChainId.Polygon]: `https://polygon-mainnet.g.alchemyapi.io/v2/`
-    };
-
-    const baseUrl = baseUrlByChainId[chainId];
-    if (!baseUrl) {
-      throw new Error(`Provider is not configured for chainId: ${chainId}`);
-    }
-
-    const providerUrl = new URL(join(baseUrl, apiKey));
-
-    return providerUrl.toString();
   }
 }
--- src/types/environment-variables.interface.ts
+++ src/types/environment-variables.interface.ts
@@ -2,4 +2,6 @@ export interface EnvironmentVariables {
   twitterBearerToken: string;
   mnemonicApiKey: string;
   ALCHEMY_API_KEY: string;
+  alchemyJsonRpcEthMainnet: string;
+  alchemyJsonRpcPolygonMainnet: string;
 }
